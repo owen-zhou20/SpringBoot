71beddc9aeca04d0245c7abf23734a762a448057
owen-zhou20 <78416546+owen-zhou20@users.noreply.github.com>
2022-05-03 23:42:11 +1000

    ssmp 1

---
+++ springboot_08_ssmp/src/main/java/com/itheima/domain/Book.java
@@ -0,0 +1,21 @@
+package com.itheima.domain;
+//lombok
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//@Getter
+//@Setter
+@Data
+@NoArgsConstructor
+public class Book {
+    private Integer id;
+    private String type;
+    private String name;
+    private String description;
+
+
+
+}
